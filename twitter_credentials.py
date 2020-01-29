b6d72333d520aeb52c4f6ea21fe24cba2da3e56e
exprt <58778200+exprt@users.noreply.github.com>
2020-01-29 03:39:54 -0800

    initial_update

---
+++ twitter_credentials.py
@@ -0,0 +1,18 @@
+################################################################################
+##################### Fill up you Twitter Credentials ##########################
+twitter_auth = {
+    'consumer_key': 'U4RLnJGn4zZPnNaAu7Rg5kIdB',
+    'consumer_secret': 'CrcwCE6AUZdgRDXJXQyB6W0kGUcitlwF2OrcjdGdEj58csi1PR',
+    'access_token': '1217316142995267584-5WzZx5UTqgy4aXz8aEkojM3zYg6KI3',
+    'access_token_secret': 'bZQ4PBTjfImatHcPUW9KpJrTmVuFyYVakQ89yCCG54URS',
+  }
+
+
+#---------------------------------------------------------------------------------------
+'''Consumer API keys
+U4RLnJGn4zZPnNaAu7Rg5kIdB (API key)
+CrcwCE6AUZdgRDXJXQyB6W0kGUcitlwF2OrcjdGdEj58csi1PR (API secret key)
+
+Access token & access token secret
+1217316142995267584-mqORPEaN0onYeNvhCgLITJJ4Chsrz5 (Access token)
+MECKAox26apZOo4vy8n6C0Jte5WHjma72c2K2ELrnWezQ (Access token secret)'''
